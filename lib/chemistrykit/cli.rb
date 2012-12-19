0cdba303f3cefedfdfb1aada0aa7c7d8bc31ab2b
Jason Wieringa <jason@jasonwieringa.com>
2012-12-19 18:13:20 -0500

    Added initial command line interface using Thor

--- lib/chemistrykit/cli.rb
+++ lib/chemistrykit/cli.rb
@@ -2,9 +2,40 @@ require "thor"
 
 module ChemistryKit
   class CLI < Thor
-    desc "ckit new <project_name>", "Creates a new ChemistryKit project"
-    def new
-      puts "new"
+    include Thor:Actions
+
+    def self.source_root
+      File.dirname(__FILE__)
     end
+
+    desc "ckit new <project_name>", "Creates a new ChemistryKit project"
+    long_desc <<-LONGDESC
+      'ckit new' will generate the a new ChemistryKit project.
+
+      You must specifiy the location and mame of the new project.
+
+      Examples:
+        ckit new cool-beans
+        ckit new .
+    LONGDESC
+
+    def new(name)
+      puts "new project"
+    end
+
+    desc "ckit generate GENERATOR [ARGS]" "Generates boiler plate files"
+    long_desc <<-LONGDESC
+      Please choose a generator below.
+
+      ckit:
+        page
+        script
+
+    LONGDESC
+
+    def genreate(name)
+      puts "generate"
+    end
+
   end
 end
