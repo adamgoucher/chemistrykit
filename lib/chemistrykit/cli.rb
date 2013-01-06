5f7a3d779450bc0c9cc72337e372b104d445e5b6
Adam Goucher <adam@goucher.ca>
2013-01-06 15:47:05 -0500

    basic creation of a new project

--- lib/chemistrykit/cli.rb
+++ lib/chemistrykit/cli.rb
@@ -5,7 +5,7 @@ module ChemistryKit
     include Thor::Actions
 
     def self.source_root
-      File.dirname(__FILE__)
+      File.join(File.dirname(__FILE__), '..')
     end
 
     desc "new [PROJECT_NAME]", "Creates a new ChemistryKit project"
@@ -13,24 +13,29 @@ module ChemistryKit
       long_desc <<-LONGDESC
         'ckit new' will generate the a new ChemistryKit project.
 
-        You must specifiy the location and mame of the new project.
+        You must specifiy the location and name of the new project.
 
         Examples:
           ckit new cool-beans
           ckit new .
       LONGDESC
     def new(name)
-      puts "made it so far"
-      Dir["templates/*"].each do |source|
-        puts "about to copy the destination"
-        destination = "templates/example/#{File.basename(source)}"
-        puts "new #{name}"
-        # FileUtils.rm(destination) if options[:force] && File.exist?(destination)
-        if File.exist?(destination)
-          puts "Skipping #{destination} because it already exists"
-        else
-          puts "Generating #{destination}"
-          FileUtils.cp(source, destination)
+      if name == "."
+        destination_root = Dir.getwd
+      else
+        Dir.mkdir(name)
+        destination_root = File.join(Dir.getwd, name)
+      end
+
+      Dir.glob(File.join(File.join(CLI.source_root, "templates", "chemistrykit"), "**/*")).each do |file|
+        path_length = File.join(CLI.source_root, "templates", "chemistrykit").length + 1
+        destination = File.join(destination_root, file[path_length .. -1])
+        if not File.exists?(destination)
+          if File.directory?(file)
+            Dir.mkdir(destination)
+          else
+            FileUtils.cp(file, destination)
+          end
         end
       end
     end
