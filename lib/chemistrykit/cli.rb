741636781f09b77a8e6366a489a5d7ba21c75bf7
Jason Wieringa <jason@jasonwieringa.com>
2013-01-07 11:51:36 -0500

    added back brew cmd, a refactoring for the 'new' cmd completed

--- lib/chemistrykit/cli.rb
+++ lib/chemistrykit/cli.rb
@@ -10,6 +10,16 @@ module ChemistryKit
 
       register(ChemistryKit::CLI::Generate, 'generate', 'generate [GENERATOR] [NAME]', 'generates something')
       register(ChemistryKit::CLI::New, 'new', 'new [NAME]', 'Creates a new ChemistryKit project')
+
+
+     desc "brew [ARGS]", "Run the Chemistry kit"
+     long_desc <<-LONGDESC
+       Runs the Chemistry kit
+     LONGDESC
+     def brew
+       puts 'in'
+     end
+
     end
   end
 end
