a4f339531cb9800498633abd07d8dc993764e935
Jason Wieringa <jason@jasonwieringa.com>
2013-01-07 11:45:25 -0500

    re-enabled generator class. Cmds work don't puke, but I broke the functionality. Needs more work; however, 'new' cmd has been refactored out of the class

--- lib/chemistrykit/cli.rb
+++ lib/chemistrykit/cli.rb
@@ -1,14 +1,14 @@
 require "thor"
-# require 'chemistrykit/generators'
+require 'chemistrykit/generators'
 require 'chemistrykit/new'
 
 module ChemistryKit
   module CLI
     class CKitCLI < Thor
 
-      # default_task :brew
+      default_task :brew
 
-     # register(ChemistryKit::CLI::Generate, 'generate', 'generate [GENERATOR] [NAME]', 'generates something')
+      register(ChemistryKit::CLI::Generate, 'generate', 'generate [GENERATOR] [NAME]', 'generates something')
       register(ChemistryKit::CLI::New, 'new', 'new [NAME]', 'Creates a new ChemistryKit project')
     end
   end
