41c767f43c07177a1251a79a326beb40abd1d5c2
jwieringa <jason@arrgyle.com>
2013-01-09 15:50:07 -0500

    temporary solution for issue #6, moved the require into the method. Yep, this is a bad idea. Will complete a refactor later

--- lib/chemistrykit/cli.rb
+++ lib/chemistrykit/cli.rb
@@ -16,10 +16,12 @@ module ChemistryKit
       long_desc <<-LONGDESC
         Runs the Chemistry kit
       LONGDESC
-      method_option :tag, :default => ['depth:shallow'], :type => :array,
+      method_option :tag, :default => ['depth:shallow'], :type => :array
       def brew
         require 'chemistrykit/config'
+        require 'chemistrykit/shared_context'
         require "#{Dir.getwd}/spec/helpers/spec_helper"
+        require 'ci/reporter/rake/rspec_loader'
 
         tags = {}
         options['tag'].each do |tag|
@@ -34,11 +36,35 @@ module ChemistryKit
           tags[filter_type][name] = value
         end
 
+        log_timestamp = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+        FileUtils.makedirs(File.join(Dir.getwd, 'logs', log_timestamp))
+        
+        ENV['CI_REPORTS'] = File.join(Dir.getwd, 'logs', log_timestamp)
+        ENV['CI_CAPTURE'] = CHEMISTRY_CONFIG['chemistrykit']['capture_output'] ? 'on' : 'off'
+
         RSpec.configure do |c|
           c.filter_run tags[:filter] unless tags[:filter].nil?
           c.filter_run_excluding tags[:exclusion_filter] unless tags[:exclusion_filter].nil?
+          c.include ChemistryKit::SharedContext
+          c.order = 'random'
         end
-        RSpec::Core::Runner.run(Dir.glob(File.join(Dir.getwd, 'spec', '**/*_spec.rb')))
+        
+        exit_code = RSpec::Core::Runner.run(Dir.glob(File.join(Dir.getwd, 'spec', '**/*_spec.rb')))
+
+        if RUBY_PLATFORM.downcase.include?("mswin")
+          require 'win32/dir'
+
+          if Dir.junction?(File.join(Dir.getwd, 'logs', 'latest'))
+            File.delete(File.join(Dir.getwd, 'logs', 'latest'))
+          end
+          Dir.create_junction(File.join(Dir.getwd, 'logs', 'latest'), File.join(Dir.getwd, 'logs', log_timestamp))
+        else
+          if File.symlink?(File.join(Dir.getwd, 'logs', 'latest'))
+            File.delete(File.join(Dir.getwd, 'logs', 'latest'))
+          end
+          File.symlink(File.join(Dir.getwd, 'logs', log_timestamp), File.join(Dir.getwd, 'logs', 'latest'))
+        end
+        exit_code
       end
     end
   end
