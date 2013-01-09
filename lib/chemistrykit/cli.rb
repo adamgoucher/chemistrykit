d41d112cc3cb02bea2593799cd3aedda9c984d9d
jwieringa <jason@arrgyle.com>
2013-01-08 20:32:52 -0500

    Incorrently renamed things. Now it's fixed

--- lib/chemistrykit/cli.rb
+++ lib/chemistrykit/cli.rb
@@ -1,6 +1,6 @@
 require "thor"
-require 'chemistry_kit/generators'
-require 'chemistry_kit/new'
+require 'chemistrykit/generators'
+require 'chemistrykit/new'
 require 'rspec'
 
 module ChemistryKit
@@ -18,7 +18,7 @@ module ChemistryKit
       LONGDESC
       option :tag, :default => 'depth:shallow', :type => :array
       def brew
-        require 'chemistry_kit/config'
+        require 'chemistrykit/config'
         require "#{Dir.getwd}/spec/helpers/spec_helper"
         
         tags = {}
