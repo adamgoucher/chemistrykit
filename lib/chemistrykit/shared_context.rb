38c7204e35789739eeb5b22e51222c6a8baebcf6
Adam Goucher <adam@goucher.ca>
2013-01-09 00:28:09 -0500

    chemistry kit wrapper around webdriver

--- lib/chemistrykit/shared_context.rb
+++ lib/chemistrykit/shared_context.rb
@@ -1,5 +1,5 @@
 require 'rspec/core/shared_context'
-require 'selenium-webdriver'
+require 'chemistrykit/webdriver'
 
 module ChemistryKit
   module SharedContext
@@ -8,7 +8,7 @@ module ChemistryKit
     before(:each) do
       executor = 'http://' + CHEMISTRY_CONFIG['webdriver']['server_host'] + ":" + CHEMISTRY_CONFIG['webdriver']['server_port'].to_s + '/wd/hub'
       capabilities = Selenium::WebDriver::Remote::Capabilities.send(CHEMISTRY_CONFIG['webdriver']['browser'])
-      @driver = Selenium::WebDriver.for(:remote, :url => executor, :desired_capabilities => capabilities)
+      @driver = ChemistryKit::WebDriver::Remote::Bridge.new(:url => executor, :desired_capabilities => capabilities)
     end
 
     after(:each) do
---
+++ lib/chemistrykit/webdriver.rb
@@ -0,0 +1,11 @@
+require 'selenium/webdriver'
+
+module ChemistryKit
+  module WebDriver
+    module Remote
+      class Bridge < Selenium::WebDriver::Remote::Bridge
+
+      end
+    end
+  end
+end
