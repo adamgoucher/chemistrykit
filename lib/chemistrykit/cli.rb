235c72d9d3f590c80a4267644ca1c0ef7649a00a
Adam Goucher <adam@goucher.ca>
2013-01-09 13:01:17 -0500

    logging ... logs are created in logs/timestamped_dir and at the end a symlink from logs/latest to the timestamped dir is recreated. its the latest symlink that you point jenkins etc at. also, for windows support it has the side effect of requiring ruby 1.9.2 or newer on that platform. which i dont think is a bad thing really.

--- lib/chemistrykit/cli.rb
+++ lib/chemistrykit/cli.rb
@@ -2,6 +2,7 @@ require "thor"
 require 'chemistrykit/generators'
 require 'chemistrykit/new'
 require 'rspec'
+require 'ci/reporter/rake/rspec_loader'
 
 module ChemistryKit
   module CLI
@@ -35,6 +36,12 @@ module ChemistryKit
           tags[filter_type][name] = value
         end
 
+        log_timestamp = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+        FileUtils.makedirs(File.join(Dir.getwd, 'logs', log_timestamp))
+        
+        ENV['CI_REPORTS'] = File.join(Dir.getwd, 'logs', log_timestamp)
+        ENV['CI_CAPTURE'] = CHEMISTRY_CONFIG['chemistrykit']['capture_output'] ? 'on' : 'off'
+
         RSpec.configure do |c|
           c.filter_run tags[:filter] unless tags[:filter].nil?
           c.filter_run_excluding tags[:exclusion_filter] unless tags[:exclusion_filter].nil?
@@ -42,6 +49,20 @@ module ChemistryKit
           c.order = 'random'
         end
         RSpec::Core::Runner.run(Dir.glob(File.join(Dir.getwd, 'spec', '**/*_spec.rb')))
+
+        if RUBY_PLATFORM.downcase.include?("mswin")
+          require 'win32/dir'
+
+          if Dir.junction?(File.join(Dir.getwd, 'logs', 'latest'))
+            File.delete(File.join(Dir.getwd, 'logs', 'latest'))
+          end
+          Dir.create_junction(File.join(Dir.getwd, 'logs', 'latest'), File.join(Dir.getwd, 'logs', log_timestamp))
+        else
+          if File.symlink?(File.join(Dir.getwd, 'logs', 'latest'))
+            File.delete(File.join(Dir.getwd, 'logs', 'latest'))
+          end
+          File.symlink(File.join(Dir.getwd, 'logs', log_timestamp), File.join(Dir.getwd, 'logs', 'latest'))
+        end
       end
     end
   end
