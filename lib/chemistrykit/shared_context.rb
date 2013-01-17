49d75737378cd766b8d9c77904767dcdcb81d5ef
Adam Goucher <adam@goucher.ca>
2013-01-16 23:00:55 -0500

    run specs in sauce ondemand

--- lib/chemistrykit/shared_context.rb
+++ lib/chemistrykit/shared_context.rb
@@ -6,8 +6,17 @@ module ChemistryKit
     extend RSpec::Core::SharedContext
 
     before(:each) do
-      executor = 'http://' + CHEMISTRY_CONFIG['webdriver']['server_host'] + ":" + CHEMISTRY_CONFIG['webdriver']['server_port'].to_s + '/wd/hub'
       capabilities = Selenium::WebDriver::Remote::Capabilities.send(CHEMISTRY_CONFIG['webdriver']['browser'])
+
+      if CHEMISTRY_CONFIG['saucelabs']['ondemand']
+        executor = "http://#{SAUCE_CONFIG['username']}:#{SAUCE_CONFIG['key']}@ondemand.saucelabs.com:80/wd/hub"
+        if CHEMISTRY_CONFIG['webdriver']['browser'] != 'chrome'
+          capabilities[:version] = CHEMISTRY_CONFIG['saucelabs']['version']
+        end
+        capabilities[:platform] = CHEMISTRY_CONFIG['saucelabs']['platform']
+      else
+        executor = 'http://' + CHEMISTRY_CONFIG['webdriver']['server_host'] + ":" + CHEMISTRY_CONFIG['webdriver']['server_port'].to_s + '/wd/hub'
+      end
       @driver = ChemistryKit::Tailored::Driver.new(:url => executor, :desired_capabilities => capabilities)
     end
 
